0cd294198c846c2c801a2bd7a58003d868f44f96
Lucas Rodrigues Seidel Silva <lucas.rodrigues28012003@gmail.com>
2024-09-24 11:39:38 -0300

    primeiro commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Customers() {
+  return (
+    <div>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <p>Customers Page</p>
+    </div>
+  );
+}
